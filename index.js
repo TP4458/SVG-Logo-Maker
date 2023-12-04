72d1d215576a901e2b116374e6a7ee7c4eba8266
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-04 22:40:42 +0000

    small bugfixes

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import inquirer from "inquirer";
 import fs from "node:fs";
 import { questions } from "./lib/questions.js";
-import { generateShape } from "./lib/generateShape.js";
+import { genShape } from "./lib/generateShape.js";
 import { renderSVG } from "./lib/renderSVG.js";
  
 function writeToFile(data) {
-    const logo = generateShape(data)
+    const logo = genShape(data)
     fs.mkdir("./dist", { recursive: true }, (error) => {if (error) throw error;})
-    fs.writeFile("./dist/logo.svg"), renderSVG(logo), (error) => error ? console.error(error) : console.log("SVG GENERATED")
+    fs.writeFile("./dist/logo.svg", renderSVG(logo), (error) => error ? console.error(error) : console.log("SVG GENERATED"));
 }
 
 async function init() {
     try {
         const data = await inquirer.prompt(questions)
+        // console.log(data)
         writeToFile(data)
     } catch (error){
         throw error;
