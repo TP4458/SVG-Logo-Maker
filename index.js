b9fb46966afab0b303d4665e8f784ed556cecc39
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-04 18:02:55 +0000

    function to write file

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ import { questions } from "./lib/questions.js";
 import { generateShape } from "./lib/generateShape.js";
 import { renderSVG } from "./lib/renderSVG.js";
  
+function writeToFile(data) {
+    const logo = generateShape(data)
+    fs.mkdir("./dist", { recursive: true }, (error) => {if (error) throw error;})
+    fs.writeFile("./dist/logo.svg"), renderSVG(logo), (error) => error ? console.error(error) : console.log("SVG GENERATED")
+}
+
+async function init() {
+    try {
+        const data = await inquirer.prompt(questions)
+        writeToFile(data)
+    } catch (error){
+        throw error;
+    }
+}
+init()
